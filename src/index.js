f7803eb8ebbf5b86a75b92f1db883a4a569a288c
ALEX <a.yuhim@mail.ru>
2024-08-23 19:46:54 +0300

    add loader & bable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './scss/style.scss';
+import './module';
 console.log('hello world!!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './index.js',
+    main: ['core-js/stable', 'regenerator-runtime/runtime', './index.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
+    clean: true,
   },
   resolve: {
     extensions: ['.js'],
@@ -24,13 +25,27 @@ module.exports = {
       template: './index.html',
     }),
     new FaviconsWebpackPlugin('fv.png'),
-    // new CopyPlugin({
-    //   patterns: [
-    //     {
-    //       from: path.resolve(__dirname, 'src', 'favicon.ico'),
-    //       to: path.resolve(__dirname, 'dist'),
-    //     },
-    //   ],
-    // }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
