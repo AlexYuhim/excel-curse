f85d023f9f71ba7cb12c544f1749a320b993dc19
ALEX <a.yuhim@mail.ru>
2024-08-24 20:42:04 +0300

    add assembly modes

--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,59 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: ['core-js/stable', 'regenerator-runtime/runtime', './index.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src', 'core'),
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new FaviconsWebpackPlugin('fv.png'),
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  const isDev = !isProd;
+  const filename = (ext) =>
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+  console.log('isProd', isProd);
+  console.log('isDev', isDev);
 
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: ['core-js/stable', 'regenerator-runtime/runtime', './index.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
       },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new FaviconsWebpackPlugin('fv.png'),
+
+      new MiniCssExtractPlugin({
+        filename: filename('css'),
+      }),
+    ],
+    devtool: isDev ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
