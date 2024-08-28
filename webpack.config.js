8237375c8867694da43b320b7c68b88412aaa66e
ALEX <a.yuhim@mail.ru>
2024-08-28 22:06:55 +0300

    delite plagin favicon install plagin copy-wp

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProd = argv.mode === 'production';
@@ -17,10 +17,17 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './index.html',
       }),
-      new FaviconsWebpackPlugin('fv.png'),
 
       new MiniCssExtractPlugin({
         filename: filename('css'),
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'src'),
+          },
+        ],
       }),
     ];
 
